b3c6256fe6c10713acf418ff430d57b2d51f53d2
social-media-project <ajitodedara68@gmail.com>
2025-01-25 23:02:48 +0530

    add host

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -2,7 +2,7 @@ const { Sequelize } = require("sequelize");
 
 // Database connection
 const sequelize = new Sequelize("social-media", "root", "Mysql@@@###123", {
-  host: "localhost",
+  host: "https://social-media-project-test.onrender.com",
   logging: false,
   dialect: "mysql",
 });
